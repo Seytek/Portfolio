b82d8b4c59022a76ef0502e6d166fabe6a41e51c
Seytek <clam.gilan@gmail.com>
2023-09-28 19:58:53 +0200

    burger menu

--- js/script.js
+++ js/script.js
@@ -1,10 +1,22 @@
 "use strict";
 
+const navItems = document.querySelector('.nav__items');
+const navBurger = document.querySelector('.nav__burger');
 
-const navBurger = document.querySelector(".nav-burger");
-const navItems = document.querySelector(".nav__items");
-
-navBurger.addEventListener("click", function() {
-    navBurger.classList.toggle("active");
-    navItems.classList.toggle("active");
+navBurger.addEventListener('click', () => {
+    navItems.classList.toggle('active');
+    navBurger.classList.toggle('active');
 });
+
+
+
+
+
+
+// const navBurger = document.querySelector(".nav-burger");
+// const navItems = document.querySelector(".nav__items");
+
+// navBurger.addEventListener("click", function() {
+//     navBurger.classList.toggle("active");
+//     navItems.classList.toggle("active");
+// });
