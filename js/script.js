2e372bb6b23c6d17e860974e6c69dd96e8f17786
Seytek <clam.gilan@gmail.com>
2023-09-30 15:58:46 +0200

    scroller in project section

--- js/script.js
+++ js/script.js
@@ -3,6 +3,8 @@
 const navItems = document.querySelector('.nav__items');
 const navBurger = document.querySelector('.nav__burger');
 
+const scrollers = document.querySelectorAll('.scroller');
+
 navBurger.addEventListener('click', () => {
     navItems.classList.toggle('active');
     navBurger.classList.toggle('active');
@@ -10,13 +12,27 @@ navBurger.addEventListener('click', () => {
 
 
 
+if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
+    addAnimation();
+}
 
+function addAnimation() {
+    scrollers.forEach(scroller => {
+         scroller.setAttribute('data-animated', true);
 
+         const scrollerInner = document.querySelector('.scroller__inner');
+         // need to get the content of the .scroller__inner
+         // add array.from so data won't change later
+         const scrollerContent = Array.from(scrollerInner.children);
 
-// const navBurger = document.querySelector(".nav-burger");
-// const navItems = document.querySelector(".nav__items");
+        // making a duplicate of our content, so there won't be any blank space
+        scrollerContent.forEach(item => {
+            const duplicatedItem = item.cloneNode(true);
+            // make sure the duplicated content won't show on screen reader
+            duplicatedItem.setAttribute("aria-hidden", true);
+            scrollerInner.appendChild(duplicatedItem);
 
-// navBurger.addEventListener("click", function() {
-//     navBurger.classList.toggle("active");
-//     navItems.classList.toggle("active");
-// });
+        })
+
+    })
+}
